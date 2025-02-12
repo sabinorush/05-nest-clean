ed60e775b244e518029e25728844f7a4abd2bd8f
Gustavo Sabino <gu.sabino@hotmail.com>
2025-02-12 17:10:19 -0300

    chore: fetch recent questions

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -5,6 +5,7 @@ import { FetchRecentQuestionsController } from './controllers/fetch-recent-quest
 import { CreateQuestionController } from './controllers/create-question.controller';
 import { DatabaseModule } from '../database/database.module';
 import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-question';
+import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions';
 
 @Module({
   imports: [DatabaseModule],
@@ -14,6 +15,6 @@ import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/crea
     CreateQuestionController,
     FetchRecentQuestionsController,
   ],
-  providers: [CreateQuestionUseCase],
+  providers: [CreateQuestionUseCase, FetchRecentQuestionsUseCase],
 })
 export class HttpModule {}
