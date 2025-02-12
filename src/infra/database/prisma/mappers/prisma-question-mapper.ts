6139027b5438a6d3e7374940eec3fbb0d14afb5d
Gustavo Sabino <gu.sabino@hotmail.com>
2025-02-12 15:34:38 -0300

    chore: implementing questionsRepository

--- src/infra/database/prisma/mappers/prisma-question-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-question-mapper.ts
@@ -1,7 +1,7 @@
+import { Question as PrismaQuestion, Prisma } from '@prisma/client';
 import { UniqueEntityID } from '@/core/entities/unique-entity-id';
 import { Question } from '@/domain/forum/enterprise/entities/question';
 import { Slug } from '@/domain/forum/enterprise/entities/value-objects/slug';
-import { Question as PrismaQuestion } from '@prisma/client';
 
 export class PrismaQuestionMapper {
   static toDomain(raw: PrismaQuestion): Question {
@@ -10,8 +10,8 @@ export class PrismaQuestionMapper {
         title: raw.title,
         content: raw.content,
         authorId: new UniqueEntityID(raw.authorId),
-        bestAnswerId: raw.bestAnswerId 
-          ? new UniqueEntityID(raw.bestAnswerId) 
+        bestAnswerId: raw.bestAnswerId
+          ? new UniqueEntityID(raw.bestAnswerId)
           : null,
         slug: Slug.create(raw.slug),
         createdAt: raw.createdAt,
@@ -19,5 +19,18 @@ export class PrismaQuestionMapper {
       },
       new UniqueEntityID(raw.id),
     );
+  }
+
+  static toPrisma(question: Question): Prisma.QuestionUncheckedCreateInput {
+    return {
+      id: question.id.toString(),
+      authorId: question.authorId.toString(),
+      bestAnswerId: question.bestAnswerId?.toString(),
+      title: question.title,
+      content: question.content,
+      slug: question.slug.value,
+      createdAt: question.createdAt,
+      updatedAt: question.updatedAt,
+    };
   }
 }
