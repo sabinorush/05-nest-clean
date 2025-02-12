6354ae9385a56af8ed0aa29cc74899a4fc77ce50
Gustavo Sabino <gu.sabino@hotmail.com>
2025-02-12 17:03:21 -0300

    chore: relation between layers of the application

--- src/infra/http/controllers/create-question.controller.ts
+++ src/infra/http/controllers/create-question.controller.ts
@@ -3,8 +3,8 @@ import { CurrentUser } from '@/infra/auth/current-user-decorator';
 import { JwtAuthGuard } from '@/infra/auth/jwt-auth.guard';
 import { UserPayload } from '@/infra/auth/jwt.strategy';
 import { ZodValidationPipe } from '@/infra/http/pipes/zod-validation-pipe';
-import { PrismaService } from '@/infra/database/prisma/prisma.service';
 import { z } from 'zod';
+import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-question';
 
 const createQuestionBodySchema = z.object({
   title: z.string(),
@@ -18,7 +18,7 @@ type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>;
 @Controller('/questions')
 @UseGuards(JwtAuthGuard)
 export class CreateQuestionController {
-  constructor(private prisma: PrismaService) {}
+  constructor(private createQuestion: CreateQuestionUseCase) {}
 
   @Post()
   async handle(
@@ -28,34 +28,11 @@ export class CreateQuestionController {
     const { title, content } = body;
     const { sub: userId } = user;
 
-    const slug = this.convertToSlug(title);
-
-    await this.prisma.question.create({
-      data: {
-        authorId: userId,
-        title,
-        content,
-        slug,
-      },
+    await this.createQuestion.execute({
+      title,
+      content,
+      authorId: userId,
+      attachmentsIds: [],
     });
-  }
-
-  private convertToSlug(title: string): string {
-    // Normalize the string to decompose accents into separate characters
-    const normalized = title.normalize('NFD');
-
-    // Remove any diacritics (accents) using a regular expression
-    const withoutAccents = normalized.replace(/[\u0300-\u036f]/g, '');
-
-    // Convert to lowercase
-    const lowercase = withoutAccents.toLowerCase();
-
-    // Replace non-alphanumeric characters (except spaces) with hyphens
-    const hyphenated = lowercase.replace(/[^a-z0-9]+/g, '-');
-
-    // Remove leading/trailing hyphens
-    const slug = hyphenated.replace(/^-+|-+$/g, '');
-
-    return slug;
   }
 }
