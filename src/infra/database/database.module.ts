4eb2f1193c7884d914f5010c73e98ff310e11b76
Gustavo Sabino <gu.sabino@hotmail.com>
2025-02-27 13:57:53 -0300

    chore: Domain Events Register

--- src/infra/database/database.module.ts
+++ src/infra/database/database.module.ts
@@ -16,6 +16,8 @@ import { QuestionCommentsRepository } from '@/domain/forum/application/repositor
 import { AttachmentsRepository } from '@/domain/forum/application/repositories/attachments-repository';
 import { PrismaAttachmentsRepository } from './prisma/repositories/prisma-attachments-repository';
 import { PrismaAnswerAttachmentsRepository } from './prisma/repositories/prisma-answers-attachments-repository';
+import { NotificationsRepository } from '@/domain/notification/application/repositories/notifications-repository';
+import { PrismaNotificationsRepository } from './prisma/repositories/prisma-notifications-repository';
 
 @Module({
   providers: [
@@ -52,6 +54,10 @@ import { PrismaAnswerAttachmentsRepository } from './prisma/repositories/prisma-
       provide: AttachmentsRepository,
       useClass: PrismaAttachmentsRepository,
     },
+    {
+      provide: NotificationsRepository,
+      useClass: PrismaNotificationsRepository,
+    },
   ],
   exports: [
     PrismaService,
@@ -63,6 +69,7 @@ import { PrismaAnswerAttachmentsRepository } from './prisma/repositories/prisma-
     AnswerCommentsRepository,
     AnswerAttachmentsRepository,
     AttachmentsRepository,
+    NotificationsRepository,
   ],
 })
 export class DatabaseModule {}
